29387a9673588299c90d2cefa146b3de913ecb72
hehongmeizengxi@163.com <hehongmeizengxi@163.com>
2016-12-10 18:17:07 +0800

    vue vue-router

---
+++ public/route-config.js
@@ -0,0 +1,7 @@
+/**
+ * Created by duoyi on 16-12-10.
+ */
+module.exports = [
+  {path: '/foo', component: ''},
+  {path: '/bar', component: ''}
+];
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 'use strict';
 const express = require('express');
+const path = require('path');
 
 const router = module.exports = express.Router();
 
 router.use('/',function (req, res, next) {
-  res.end('hello world');
+  res.sendFile(path.join(__dirname,'../public/html/index.html'));
 });
